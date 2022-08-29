207497c21681ed52a8b631ffa97e7a6cde07dc65
Ricardo Colom <89496569+ricardocolom64@users.noreply.github.com>
2022-08-29 16:23:22 -0400

    Time: 0 ms (100.00%), Space: 42.6 MB (22.80%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -27,7 +27,6 @@ class Solution {
             return;
         
         searchTree(node.left);
-        System.out.println(node.val);
         inOrder.add(node.val);
         searchTree(node.right);
     }
