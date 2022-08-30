79c60d668bfb52fd37784e5009d96053354d9505
Ricardo Colom <89496569+ricardocolom64@users.noreply.github.com>
2022-08-30 02:36:49 -0400

    Time: 1170 ms (5.09%), Space: 68.8 MB (89.00%) - LeetHub

---
+++ 1609-even-odd-tree/1609-even-odd-tree.java
@@ -0,0 +1,130 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isEvenOddTree(TreeNode root) {
+        Queue<TreeNode> nodeQ = new LinkedList<TreeNode>();
+        
+        if(root.val % 2 == 0)
+            return false;
+        
+        nodeQ.add(root);
+        nodeQ.add(null);
+        
+        ArrayList<ArrayList<Integer>> levelNums = new ArrayList<ArrayList<Integer>>();
+        
+        levelNums.add(new ArrayList<>());
+        
+        int currLevel = 0;
+        
+        while(!nodeQ.isEmpty())
+        {
+            TreeNode head = nodeQ.peek();
+            
+            if(head != null)
+            {
+                if(levelNums.size() == currLevel)
+                {
+                    levelNums.add(new ArrayList<>());
+                }
+                levelNums.get(currLevel).add(nodeQ.peek().val);
+                System.out.println("Adding " + nodeQ.peek().val + " to " + currLevel);
+                
+                if(head.left != null)
+                    nodeQ.add(head.left);
+                if(head.right != null)
+                    nodeQ.add(head.right);
+            }
+            else
+            {
+                if(nodeQ.size() > 1)
+                {
+                    nodeQ.add(null);
+                    System.out.println("----");
+                    currLevel++;
+                }
+            }
+            nodeQ.remove();
+        }
+        
+        boolean currLevelOdd = true;
+        boolean currLevelIncreasing = true;
+        
+        for(int i = 0; i < levelNums.size(); ++i)
+        {
+            int currIndex = 0;
+            //Even level
+            if(i % 2 == 0)
+            {
+                System.out.println(i + " -> " + isOddAndIncreasing(levelNums.get(i)));
+                if(!isOddAndIncreasing(levelNums.get(i)))
+                   return false;
+            }
+            else
+            {
+                System.out.println(i + " -> " + isEvenAndDecreasing(levelNums.get(i)));
+                if(!isEvenAndDecreasing(levelNums.get(i)))
+                   return false;
+            }
+        }        
+        return true;
+    }
+    
+    public boolean isOddAndIncreasing(ArrayList<Integer> list)
+    {
+        if(list.size() > 1)
+        {
+            for(int i = 0; i < list.size() - 1; ++i)
+            {
+                if(list.get(i) >= list.get(i + 1))
+                    return false;
+                if(list.get(i) % 2 == 0)
+                    return false;
+            }
+            
+            if(list.get(list.size() - 1) % 2 == 0)
+                return false;
+        }
+        else
+        {
+            if(list.get(0) % 2 == 0)
+                return false;
+        }
+        return true;
+    }
+    
+    public boolean isEvenAndDecreasing(ArrayList<Integer> list)
+    {
+        if(list.size() > 1)
+        {
+            for(int i = 0; i < list.size() - 1; ++i)
+            {
+                if(list.get(i) <= list.get(i + 1))
+                    return false;
+                if(list.get(i) % 2 == 1)
+                    return false;
+            }
+            
+            if(list.get(list.size() - 1) % 2 == 1)
+                return false;
+        }
+        else
+        {
+            if(list.get(0) % 2 == 1)
+                return false;
+        }
+        return true;
+    }
+}
