a943191dae32f3709b783a762af38506245c1d0d
Ricardo Colom <89496569+ricardocolom64@users.noreply.github.com>
2022-09-01 20:25:47 -0400

    Time: 7 ms (5.28%), Space: 41.3 MB (85.87%) - LeetHub

---
+++ 27-remove-element/27-remove-element.java
@@ -0,0 +1,33 @@
+/*
+
+[1,2,2,3,5,6,1,3,5,7,2,6]
+
+[1,3,5,6,1,3,5,7]
+
+*/
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        
+        int j = nums.length - 1;
+        int occ = 0;
+        
+        for(int i = 0; i < nums.length; ++i)
+        {
+            if(nums[i] == val)
+            {
+                occ++;
+                while(nums[j] == val && j > 0)
+                {
+                    System.out.println("nums[" + j + "] is " + val);
+                    --j;
+                }
+                nums[i] = nums[j];
+                if(j > 0)
+                    --j;
+            }
+        }
+        
+        return nums.length - occ;
+    }
+}
