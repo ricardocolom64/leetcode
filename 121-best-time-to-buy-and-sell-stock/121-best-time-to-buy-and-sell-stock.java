3c1ad8859b9f045d7f92dda4624330373b5d4d41
Ricardo Colom <89496569+ricardocolom64@users.noreply.github.com>
2022-09-11 00:55:46 -0400

    Time: 2383 ms (5.67%), Space: 123.4 MB (5.10%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        if(prices.length <= 1)
+            return 0;
+        
+        int maxDiff = prices[1] - prices[0];
+        int min = prices[0];
+        
+        for(int i = 0; i < prices.length; ++i)
+        {
+            System.out.println(prices[i]);
+            if(prices[i] < min)
+            {
+                min = prices[i];
+                //System.out.println("New min: " + prices[i]);
+            }
+            if(prices[i] - min > maxDiff)
+            {
+                maxDiff = prices[i] - min;
+                //System.out.println("New maxDiff: " + maxDiff);
+            }
+                
+        }
+        
+        return maxDiff;
+    }
+}
