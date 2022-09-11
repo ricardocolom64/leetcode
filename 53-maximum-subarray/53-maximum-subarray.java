0a916bd6a10d2350b0c288ddcc6b5221eb2a94dd
Ricardo Colom <89496569+ricardocolom64@users.noreply.github.com>
2022-09-11 02:32:10 -0400

    Time: 2 ms (74.56%), Space: 73.7 MB (58.03%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         int currSum = 0;
-        int maxSum = nums[0];
+        int maxSum = Integer.MIN_VALUE;
         
         // for(int i = 0; i < nums.length; ++i)
         // {
