63fa9fe1d9492d54e796f7ee02c24c6bd865bb0e
Ricardo Colom <89496569+ricardocolom64@users.noreply.github.com>
2022-09-11 02:57:44 -0400

    Time: 1 ms (97.71%), Space: 44.9 MB (51.43%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -1,33 +1,52 @@
 class Solution {
     public int maxProduct(int[] nums) {
         
-        int maxsum = nums[0];
-        int cursum = 1;
-        if (nums.length == 1) {
-            return nums[0];
+        int currProd = 1;
+        int maxProd = nums[0];
+        
+        // Kadane?
+        
+        for(int i = 0; i < nums.length; ++i)
+        {
+            currProd *= nums[i];
+            
+            if(currProd > maxProd)
+                maxProd = currProd;
+            
+            if(nums[i] == 0)
+                currProd = 1;
         }
-        for (int i = 0; i < nums.length; i++) {
-            cursum = cursum * nums[i];
-
-            if (cursum > maxsum) {
-                maxsum = cursum;
-            }
-            if (cursum == 0) {
-                cursum = 1;
-            }
+        
+        currProd = 1;
+        
+        for(int i = nums.length - 1; i > 0; --i)
+        {
+            currProd *= nums[i];
+            
+            if(currProd > maxProd)
+                maxProd = currProd;
+            
+            if(nums[i] == 0)
+                currProd = 1;
         }
-        cursum = 1;
-        for (int i = nums.length - 1; i > 0; i--) {
-            cursum = cursum * nums[i];
-
-            if (cursum > maxsum) {
-                maxsum = cursum;
-            }
-            if (cursum == 0) {
-                cursum = 1;
-            }
-        }
-        return maxsum;
+        
+        
+//         // Brute Force
+        
+//         for(int i = 0; i < nums.length; ++i)
+//         {
+//             currProd = 1;
+            
+//             for(int j = i; j < nums.length; ++j)
+//             {
+//                 currProd *= nums[j];
+                
+//                 if(currProd > maxProd)
+//                     maxProd = currProd;
+//             }
+//         }
+        
+        return maxProd;
         
     }
 }
