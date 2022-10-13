04f89e4d9cb8b0f1958729fe63c986145948bd49
Ricardo Colom <89496569+ricardocolom64@users.noreply.github.com>
2022-10-13 18:42:40 -0400

    Time: 0 ms (100.00%), Space: 42.7 MB (99.00%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,16 +1,34 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
+//         Arrays.sort(nums);
         
-        for(int i = 1 ; i < nums.length; ++i)
+//         for(int i = 1 ; i < nums.length; ++i)
+//         {
+//             if(nums[i] - nums[i-1] != 1)
+//                 return nums[i] - 1;
+//         }
+        
+//         if(nums[nums.length - 1] == nums.length)
+//             return 0;
+        
+//         return nums.length;
+        
+        int[] newArr = new int[nums.length + 1];
+        
+        for(int i = 0; i < nums.length; ++i)
         {
-            if(nums[i] - nums[i-1] != 1)
-                return nums[i] - 1;
+            newArr[nums[i]] = 1;
+            //System.out.println("i = " + i + "\tnewArr = " + Arrays.toString(newArr));
         }
         
-        if(nums[nums.length - 1] == nums.length)
-            return 0;
+        for(int i = 0; i < newArr.length; ++i)
+        {
+            if(newArr[i] == 0)
+                return i;
+        }
         
-        return nums.length;
+        return 0;
+        
+        
     }
 }
