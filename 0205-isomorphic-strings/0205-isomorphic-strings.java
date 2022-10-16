494c27860925f858a9c8c392e22a51f06febf2ac
Ricardo Colom <89496569+ricardocolom64@users.noreply.github.com>
2022-10-16 17:21:51 -0400

    Time: 32 ms (26.79%), Space: 43.1 MB (49.30%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -7,23 +7,23 @@ class Solution {
         {
             if(smap.containsKey(s.charAt(i)))
             {
-                System.out.println("smap contains " + s.charAt(i) + "\tval: " + smap.get(s.charAt(i)));
+                //System.out.println("smap contains " + s.charAt(i) + "\tval: " + smap.get(s.charAt(i)));
                 if(smap.get(s.charAt(i)) != t.charAt(i))
                     return false;
             }
             if(tmap.containsKey(t.charAt(i)))
             {
-                System.out.println("tmap contains " + t.charAt(i) + "\tval: " + tmap.get(t.charAt(i)));
+                //System.out.println("tmap contains " + t.charAt(i) + "\tval: " + tmap.get(t.charAt(i)));
                 if(tmap.get(t.charAt(i)) != s.charAt(i))
                     return false;
             }
             smap.put(s.charAt(i), t.charAt(i));
-            System.out.println("smap: " + s.charAt(i) + "\tval: " + t.charAt(i));
+            //System.out.println("smap: " + s.charAt(i) + "\tval: " + t.charAt(i));
             tmap.put(t.charAt(i), s.charAt(i));
-            System.out.println("tmap: " + t.charAt(i) + "\tval: " + s.charAt(i));
+            //System.out.println("tmap: " + t.charAt(i) + "\tval: " + s.charAt(i));
         }
         
-        System.out.println("All characters mapped");
+        //System.out.println("All characters mapped");
         
         for(int i = 0; i < s.length(); ++i)
         {
