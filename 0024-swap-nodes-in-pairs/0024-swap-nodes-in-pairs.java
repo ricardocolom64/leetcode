f8bf25e36d3cb78eeeb67804d34ff2f092706a90
Ricardo Colom <89496569+ricardocolom64@users.noreply.github.com>
2022-10-16 04:08:32 -0400

    Time: 43 ms (5.38%), Space: 43.2 MB (6.44%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -0,0 +1,88 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        
+        if(head == null)
+            return null;
+        
+        if(head.next == null)
+            return head;
+        
+        ListNode curr = head;
+        
+        ArrayList<ListNode> oddNodes = new ArrayList<>();
+        ArrayList<ListNode> evenNodes = new ArrayList<>();
+        
+        int amt = 1;
+        
+        while(curr != null)
+        {
+            if(amt % 2 == 1)
+                oddNodes.add(curr);
+            else
+                evenNodes.add(curr);
+            
+            amt++;
+            curr = curr.next;
+        }
+        
+        ListNode result = evenNodes.get(0);
+        
+        curr = result;
+        
+        int oddIndex = 0;
+        int evenIndex = 0;
+        
+        oddNodes.get(oddNodes.size() - 1).next = null;
+        evenNodes.get(evenNodes.size() - 1).next = null;
+        
+        System.out.println(evenNodes.size());
+        
+        if(evenNodes.size() == 1 && oddNodes.size() == 1)
+        {
+            result.next = oddNodes.get(0);
+            result.next.next = null;
+            
+            return result;
+        }
+        
+        for(int i = 0; i < oddNodes.size() + evenNodes.size(); ++i)
+        {
+            System.out.println("i: " + i);
+            System.out.println("curr: " + curr.val);
+            if(i % 2 == 0)
+            {
+                curr.next = oddNodes.get(i / 2);
+            }                
+            else
+            {
+                if((i / 2) + 1 == evenNodes.size())
+                {
+                    if((i / 2) + 2 == oddNodes.size())
+                        curr.next = oddNodes.get((i / 2) + 1);
+                    break;                    
+                }
+                else
+                    curr.next = evenNodes.get((i / 2) + 1);
+            }
+            
+            System.out.println("curr.next: " + curr.next.val);
+            System.out.println("");
+            
+            curr = curr.next;
+        }
+        
+        System.out.println(result.next.next.val);
+        
+        return result;
+    }
+}
