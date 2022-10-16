3741e3562f7fec11d98c69f734b76cae7257b50d
Ricardo Colom <89496569+ricardocolom64@users.noreply.github.com>
2022-10-16 00:57:27 -0400

    Time: 58 ms (18.41%), Space: 53.8 MB (17.73%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        String longest = "";
+        String curr = "";
+        
+        for(int i = 0; i < s.length(); ++i)
+        {
+            String currentCharacterToString = "" + s.charAt(i);
+            if(curr.contains(currentCharacterToString))
+            {
+                //System.out.println("curr, " + curr + ", contains " + s.charAt(i));
+                if(curr.length() > longest.length())
+                {
+                    longest = curr;
+                    //System.out.println("longest: " + longest);
+                }
+                curr = curr.substring(curr.indexOf(s.charAt(i)) + 1, curr.length());
+                //System.out.println("curr after substring: " + curr);
+            }
+            
+            curr += s.charAt(i);
+            //System.out.println("Adding " + s.charAt(i) + " to curr");
+        }
+        
+        if(curr.length() > longest.length())
+            longest = curr;
+        
+        //System.out.println("curr after finishing: " + curr);
+        //System.out.println("longest after finishing: " + longest);
+        
+        return longest.length();
+    }
+}
