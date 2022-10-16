ffea02037ec034eaa8dfeef249e927d86417dbcd
Ricardo Colom <89496569+ricardocolom64@users.noreply.github.com>
2022-10-16 01:58:10 -0400

    Time: 16 ms (50.15%), Space: 45.3 MB (25.06%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0)
+            return false;
+        
+        String num = "" + x;
+        
+        for(int i = 0; i < num.length() / 2; ++i)
+        {
+            if(num.charAt(i) != num.charAt(num.length() - 1 - i)) 
+                return false;
+        }            
+            
+        return true;
+    }
+}
