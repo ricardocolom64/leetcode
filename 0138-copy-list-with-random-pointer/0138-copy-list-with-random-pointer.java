30073be2772985a843a856752196299a59297e70
Ricardo Colom <89496569+ricardocolom64@users.noreply.github.com>
2022-10-17 03:49:33 -0400

    Time: 3 ms (8.47%), Space: 45.3 MB (69.74%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -26,6 +26,8 @@ class Solution {
         ArrayList<Node> originalNodes = new ArrayList<Node>();
         ArrayList<Node> copyNodes = new ArrayList<Node>();
         
+        // loop through each copy node to make sure that its nexts are the right values
+        // also populate ArrayLists to get the indexes of random nodes for later
         while(originalCurr != null)
         {
             originalNodes.add(originalCurr);
@@ -42,17 +44,16 @@ class Solution {
             copyCurr = copyCurr.next;
         }
         
-        copyCurr = copyHead;
+        copyCurr = copyHead;        
+        originalCurr = head;
         int pos = 0;
         
-        originalCurr = head;
-        
+        // Iterates through originalNodes to find the index of its random node in the list, then make the copyCurr's random the respective node at its copyNodes list
         while(originalCurr != null){
             
             if(originalCurr.random != null)
             {
                 pos = originalNodes.indexOf(originalCurr.random);
-                //System.out.println("pos of random is " + pos);
                 copyCurr.random = copyNodes.get(pos);
             }
             
