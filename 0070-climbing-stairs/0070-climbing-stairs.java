bf3dd4b38bf366e1cfcbc3bbb83651bf75864945
Ricardo Colom <89496569+ricardocolom64@users.noreply.github.com>
2022-10-17 02:54:12 -0400

    Time: 0 ms (100.00%), Space: 41.5 MB (8.93%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -7,17 +7,15 @@ class Solution {
         
         int p1 = 1;
         int p2 = 1;
-        int sum = 1;
         
         for(int i = n; i > 1; --i)
         {
-            sum = p1 + p2;
-            
+            int sum = p1 + p2;            
             
             p2 = p1;
             p1 = sum;
         }
         
-        return sum;
+        return p1;
     }
 }
